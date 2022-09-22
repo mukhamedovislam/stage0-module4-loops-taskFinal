56d42a7be578cc7bece9eac7a2448a71aee6bd34
mukhamedovislam <bozuy24@gmail.com>
2022-09-22 08:49:22 +0400

    Solutions #2

--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -3,13 +3,13 @@ package school.mjc.stage0.loops.finalTask;
 public class Pyramid {
     public void printPyramid(int cathetusLength) {
 	    for(int i = 1; i <= cathetusLength; i++){
-		for(int j = cathethusLength; j > 1; j--){
+		for(int j = cathetusLength; j > 1; j--){
 			System.out.print(" ");
 		}
 		for(int k = i; k != 0; k--){
 			System.out.print(k);
 		}
-		cathethusLength--;
+		cathetusLength--;
 		for(int l = 2; l <= i; l++){
 			System.out.print(l);
 		}
